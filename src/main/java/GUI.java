0850fdb7df8ab901bb465e02c7499686562e5481
jc-uni <jonas.claesson@unicus.com>
2021-11-26 15:38:12 +0100

    continued work on GUI, merged defined_formulas

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -6,22 +6,39 @@ public class GUI {
         String input;
         var scanner = new Scanner(System.in);
 
-        System.out.println("Hello" +"\r\n" +"What would you like to calculate?");
+        System.out.println("Hello" +"\r\n" +"What would you like to calculate (0-8)?");
         input = scanner.nextLine();
 
-        switch (input){
-            case "0" -> System.out.println("area, circle");
-            case "1" -> System.out.println("area, triangle");
-            case "2" -> System.out.println("area, rectangle");
-            case "3" -> System.out.println("circumference, circle");
-            case "4" -> System.out.println("circumference, triangle");
-            case "5" -> System.out.println("circumference, rectangle");
-            case "6" -> System.out.println("volume, sphere");
-            case "7" -> System.out.println("volume, pyramid");
-            case "8" -> System.out.println("volume, rectangle");
+        switch (input) {
+            case "0" -> {
+                System.out.println("area, circle" + "\r\n" + "Radius:");
+                System.out.println("The area is: " + Calc.RoundAnswer(Calc.AreaCircle(scanner.nextDouble())));
+            }
+            case "1" -> {
+                System.out.println("area, triangle " +"\r\n" +"dimensions:");
+                System.out.println();
+            }
+            case "2" -> {
+                System.out.println("area, rectangle" +"\r\n" +"dimensions:");
+            }
+            case "3" -> {
+                System.out.println("circumference, circle" +"\r\n" +"Radius:");
+            }
+            case "4" -> {
+                System.out.println("circumference, triangle" +"\r\n" +"dimensions:");
+            }
+            case "5" -> {
+                System.out.println("circumference, rectangle" +"\r\n" +"dimensions:");
+            }
+            case "6" -> {
+                System.out.println("volume, sphere" +"\r\n" +"Radius:");
+            }
+            case "7" -> {
+                System.out.println("volume, pyramid" +"\r\n" +"dimensions:");
+            }
+            case "8" -> {
+                System.out.println("volume, rectangle" +"\r\n" +"dimensions:");
+            }
         }
     }
-
-
-
 }
